f0d5bd0ef261295b6fa1cf1238caf5d7dcff1394
Dave Martin <djtfmartin@gmail.com>
2014-12-07 08:38:42 +1100

    temporary include to fix build
    temporary include to fix build - need to update aka-web-theme

---
+++ src/groovy/au/org/ala/web/UserDetails.groovy
@@ -0,0 +1,22 @@
+package au.org.ala.web
+
+/**
+ * Copied from ala-web-theme.
+ *
+ * FIXME - remove and roll changes into plugin.
+ *
+ * @author "Nick dos Remedios <Nick.dosRemedios@csiro.au>"
+ */
+class UserDetails {
+
+    public static final String REQUEST_USER_DETAILS_KEY = 'request.user.details'
+
+    String displayName // full name
+    String userName    // email
+    String userId      // String id
+
+    @Override
+    public String toString() {
+        "[ userId: ${userId}, userName: ${userName}, displayName: ${displayName} ]"
+    }
+}
