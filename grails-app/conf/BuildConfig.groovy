082670c074e4129cfb166aea262483f8735a1d1e
m-r-c <m-r-c@users.noreply.github.com>
2016-02-15 22:18:13 +1100

    uncomments ala-map; excludes release transitive dependency
    still experimenting, trying to sort out the maven-deploy linkageerror on HttpEntity

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -70,7 +70,9 @@ grails.project.dependency.resolution = {
         compile(":jasper:1.11.0") {
             excludes 'jasperreports'
         }
-   //     compile ":ala-map:1.2-SNAPSHOT"
+        compile(":ala-map:1.2-SNAPSHOT") {
+            excludes "release", "rest-client-builder"
+        }
 
         runtime ":resources:1.2.14"
         runtime(":ala-bootstrap3:1.1") {
