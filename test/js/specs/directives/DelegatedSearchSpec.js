7a57c9cab9c4bf89aa7d80042f7d43d65425f990
m-r-c <m-r-c@users.noreply.github.com>
2016-03-18 13:30:54 +1100

    #248 adds support for including archived profiles in the general search

--- test/js/specs/directives/DelegatedSearchSpec.js
+++ test/js/specs/directives/DelegatedSearchSpec.js
@@ -73,7 +73,7 @@ describe('Directive: DelegatedSearch', function () {
         scope.delSearchCtrl.search();
         scope.$digest();
 
-        expect(sessionStorage.delegatedSearches).toEqual({"opusId1": "test"});
+        expect(sessionStorage.delegatedSearches).toEqual({"opusId1": {term: "test", "searchOptions": {"includeArchived": false}}});
     });
 
     it("should populate the delegatedSearches object of the sessionStorage for the 'all' search when search is invoked and there is no opus", function() {
@@ -83,7 +83,7 @@ describe('Directive: DelegatedSearch', function () {
         scope.delSearchCtrl.search();
         scope.$digest();
 
-        expect(sessionStorage.delegatedSearches).toEqual({"all": "test"});
+        expect(sessionStorage.delegatedSearches).toEqual({"all": {term: "test", "searchOptions": {"includeArchived": false}}});
     });
 
     it("should redirect to the opus home page when searching within an opus", function() {
