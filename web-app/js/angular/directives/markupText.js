aaf66ede62cf8cbe7405af04c5ff33ee5fdd5b47
m-r-c <m-r-c@users.noreply.github.com>
2016-03-22 12:53:52 +1100

    #171 fixes thumbnail image url matching

--- web-app/js/angular/directives/markupText.js
+++ web-app/js/angular/directives/markupText.js
@@ -9,7 +9,7 @@
  */
 profileEditor.directive('markupText', function ($compile, util) {
     // matches elements like <img src=\"http://profiles.ala.org.au/opus/8aa27a28-e38f-4048-8349-9102e5164948/profile/b484380d-4d04-4fcc-bb71-c38f33829ead/image/fa09b7f4-4211-413a-a063-07c7ac6468ae.jpg?type&#61;PRIVATE\" />
-    var localImageRegex = new RegExp("<img(.*?)src=(.*?)/image/(" + util.UUID_REGEX_PATTERN + ")(\.[a-z]{3,4}[\?\"'])(.*?)>", "gi");
+    var localImageRegex = new RegExp("<img(.*?)src=(.*?)/image/thumbnail/(" + util.UUID_REGEX_PATTERN + ")(\.[a-z]{3,4}[\?\"'])(.*?)>", "gi");
 
     // matches elements like <img src="http://images.ala.org.au/image/proxyImageThumbnail?imageId&#61;8ee9969f-3122-43f1-a624-dc423e9b94bb" />
     var remoteImageRegex1 = new RegExp("<img(.*?)src=(.*?)\?imageId(=|(&#61;))(" + util.UUID_REGEX_PATTERN + ")(.*?)>", "gi");
