441f55f50954d9c94157668baf8614cd0939e348
shi131 <paul.shields@csiro.au>
2016-05-03 16:32:45 +1000

    #329 Images - don't delete private image if upload fails, supporting tests

--- grails-app/services/au/org/ala/profile/hub/BiocacheService.groovy
+++ grails-app/services/au/org/ala/profile/hub/BiocacheService.groovy
@@ -4,6 +4,13 @@ import au.org.ala.ws.service.WebService
 import groovy.json.JsonSlurper
 import org.springframework.web.multipart.MultipartFile
 
+import java.nio.file.Files
+import java.nio.file.Path
+
+import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+
 class BiocacheService {
 
     static final int DEFAULT_BIOCACHE_PAGE_SIZE = 50 // the biocache default is only 10, we want more than that
@@ -27,19 +34,43 @@ class BiocacheService {
         webService.get("${biocacheImageSearchUrl}?q=${imagesQuery}&fq=multimedia:Image&format=json&im=true&pageSize=${DEFAULT_BIOCACHE_PAGE_SIZE}")
     }
 
-    def uploadImage(String opusId, String profileId, String dataResourceId, file, Map metadata) {
-        String imageId = UUID.randomUUID()
 
-        String filename
-        File tempDir = new File("${grailsApplication.config.temp.file.location}")
+
+    String copyFileForUpload(String imageId, def file, File tempDir) {
+        String filename = ''
+        //Images sent directly to central service on upload
         if (file instanceof MultipartFile) {
             String extension = file.originalFilename.substring(file.originalFilename.lastIndexOf("."))
             filename = "${imageId}${extension}"
             file.transferTo(new File(tempDir, "/${filename}"))
+            //Private and staged images are stored relative to this application, and may be sent to central image service later
         } else if (file instanceof File) {
             filename = file.getName()
-            file.renameTo(new File(tempDir, file.getName()));
+//        file.renameTo(new File(tempDir, file.getName()))
+            //Make defensive copy in case fails
+            File fileCopy = new File(tempDir, "/${filename}")
+            Path target = fileCopy.toPath()
+            Path source = file.toPath()
+            Files.copy(source, target, REPLACE_EXISTING, COPY_ATTRIBUTES)
         }
+        filename
+    }
+
+    /**
+     * Upload works by sending metadata to a central image service which then comes back to fetch the image based
+     *  on information in this metadata??
+     *
+     * @param opusId - collection
+     * @param profileId - the item within the collection
+     * @param dataResourceId
+     * @param file - can be a File from disk or a MultipartFile streamed directly
+     * @param metadata - information about the image
+     * @return Map response from the webservice including statusCode and resp
+     */
+    def uploadImage(String opusId, String profileId, String dataResourceId,  file, Map metadata) {
+        String imageId = UUID.randomUUID()
+        File tempDir = new File("${grailsApplication.config.temp.file.location}")
+        String filename = copyFileForUpload(imageId,file,tempDir)
 
         metadata.multimedia[0].identifier = "${grailsApplication.config.grails.serverURL}/opus/${enc(opusId)}/profile/${enc(profileId)}/file/${enc(filename)}"
 
@@ -53,7 +84,7 @@ class BiocacheService {
 
         // TODO: REMOVE THIS - IT IS FOR TESTING ONLY!!!!
         String hostname = InetAddress.getLocalHost().hostName
-        if ((hostname != 'nci-profiles-prod'|| hostname == "nci-profiles" || hostname == "nci-profiles-dev" || hostname == "maccy-bm")
+        if ((hostname != 'nci-profiles-prod' || hostname == "nci-profiles" || hostname == "nci-profiles-dev" || hostname == "maccy-bm")
                 && grailsApplication.config.test.collectory.drid.mappings) {
             Map drIdMapping = new JsonSlurper().parseText(grailsApplication.config.test.collectory.drid.mappings)
 
@@ -63,6 +94,8 @@ class BiocacheService {
 
         webService.post("${grailsApplication.config.image.upload.url}${dataResourceId}?apiKey=${grailsApplication.config.image.upload.apiKey}", metadata)
     }
+
+
 
     private static enc(String str) {
         URLEncoder.encode(str, "utf-8")
