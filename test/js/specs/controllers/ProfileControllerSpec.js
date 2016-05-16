32d94f2de149a4d97dd3d6240c81bcd0696bc812
m-r-c <m-r-c@users.noreply.github.com>
2016-05-16 17:23:25 +1000

    #348 fixed the temporarily broken ProfileControllerSpec.js: the change to the .travis file worked.

--- test/js/specs/controllers/ProfileControllerSpec.js
+++ test/js/specs/controllers/ProfileControllerSpec.js
@@ -88,7 +88,7 @@ describe("ProfileController tests", function () {
         scope.profileCtrl.loadProfile();
         scope.$apply();
 
-        expect(profileService.getProfile).not.toHaveBeenCalled();
+        expect(profileService.getProfile).toHaveBeenCalled();
         expect(scope.profileCtrl.profile).toBeDefined();
     });
 
