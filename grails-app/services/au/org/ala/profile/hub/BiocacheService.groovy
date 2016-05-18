aab268a61f1b98e05f1c66a21914a0738ada35c6
m-r-c <m-r-c@users.noreply.github.com>
2016-05-19 09:05:42 +1000

    throws an exception if the image resource option is not recognised

--- grails-app/services/au/org/ala/profile/hub/BiocacheService.groovy
+++ grails-app/services/au/org/ala/profile/hub/BiocacheService.groovy
@@ -1,6 +1,7 @@
 package au.org.ala.profile.hub
 
 import au.org.ala.ws.service.WebService
+import com.sun.javaws.exceptions.InvalidArgumentException
 import groovy.json.JsonSlurper
 import org.springframework.web.multipart.MultipartFile
 
@@ -79,7 +80,7 @@ class BiocacheService {
                     query = "${searchIdentifier} AND (data_resource_uid:${opus.dataResourceUid} OR data_resource_uid:${config.imageSources?.join(" OR data_resource_uid:")})"
                     break
                 default:
-                    query = "${searchIdentifier}"
+                    throw new InvalidArgumentException("${config.imageResourceOption} is not a recognized DataResourceOption")
             }
         } else if (opus?.dataResourceUid) {
             query = "${searchIdentifier} AND data_resource_uid:${opus.dataResourceUid}"
