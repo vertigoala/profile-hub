f26e69ca2f6316d39d2d619da62dec4706c3b0f6
shi131 <paul.shields@csiro.au>
2016-05-23 12:03:18 +1000

    #310 -Image pagination: unit test add paramaterised test for Staged images

--- test/unit/au/org/ala/profile/hub/ImagePagingSpec.groovy
+++ test/unit/au/org/ala/profile/hub/ImagePagingSpec.groovy
@@ -1,6 +1,7 @@
 package au.org.ala.profile.hub
 
 import grails.test.mixin.TestFor
+import spock.lang.IgnoreRest
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -30,6 +31,8 @@ class ImagePagingSpec extends Specification {
     List privateImages = ["privateImage1", "privateImage2", "privateImage3", "privateImage4", "privateImage5", "privateImage6", "privateImage7", "privateImage8", "privateImage9", "privateImage10", "privateImage11", "privateImage12"]
     @Shared
     List privateImagesComplex = [[imageId: 'imageId1'], [imageId: 'imageId2'], [imageId: 'imageId3'], [imageId: 'imageId4'], [imageId: 'imageId5'], [imageId: 'imageId6'], [imageId: 'imageId7'], [imageId: 'imageId8'], [imageId: 'imageId9'], [imageId: 'imageId10'], [imageId: 'imageId11'], [imageId: 'imageId12']]
+    @Shared
+    List stagedImagesComplex = [[imageId: 'imageId1'], [imageId: 'imageId2'], [imageId: 'imageId3'], [imageId: 'imageId4'], [imageId: 'imageId5'], [imageId: 'imageId6'], [imageId: 'imageId7'], [imageId: 'imageId8'], [imageId: 'imageId9'], [imageId: 'imageId10'], [imageId: 'imageId11'], [imageId: 'imageId12']]
     //List [[:],[:],[:],[:]]
     @Shared
     List publishedImages = [[image: "published1"], [image: "published2"], [image: "published3"], [image: "published4"], [image: "published5"], [image: "published6"], [image: "published7"], [image: "published8"], [image: "published9"], [image: "published10"], [image: "published11"]]
@@ -153,16 +156,19 @@ class ImagePagingSpec extends Specification {
 
     }
 
-    def "Private Image Paging - multiple pages when there are no published images, only private images"() {
-        given: "a profile containing private images"
+
+    def "Image Paging - multiple pages when there are no published images, only Private or only Staged "() {
+        given: "a profile containing images"
         profileService = Mock(ProfileService)
-        profileService.getProfile(_, _, _) >> [profile: [scientificName: 'Olympia', uuid: 'profile1', privateImages: privateImagesComplex], opus: [keepImagesPrivate: true, uuid: 'collection1']]
+        profileService.getProfile(_, _, _) >> profileResponse
         imageService.setProfileService(profileService)
-        imageService.setBiocacheService(biocacheServiceMockNoImages)
+        imageService.setBiocacheService(biocacheService)
         Integer offset = pageSize * 0
         Integer offset2 = pageSize * 1
         Integer offset3 = pageSize * 2
         Integer offset4 = pageSize * 3
+        Integer totalNumberOfImages = profileResponse.profile.privateImages.size()
+        if(totalNumberOfImages == 0){ totalNumberOfImages = profileResponse.profile.stagedImages.size()}
         when: "we request pages of all images sequentially"
         def alaResponse = imageService.retrieveImagesPaged('collection1', 'profile1', true, '', false, true, pageSize.toString(), String.valueOf(offset))
         List imagesPage1 = alaResponse.resp.images
@@ -174,23 +180,27 @@ class ImagePagingSpec extends Specification {
         List imagesPage4 = alaResponse4.resp.images
         Integer count = alaResponse.resp.count
         then: "the first page has the correct number of images"
-        assert (imagesPage1.size() == pageSize)
+        assertTrue(test_description+' - page 1 does not have the correct number of images', imagesPage1.size() == pageSize)
         and: "the first image of the first page is the first image available"
-        assert imagesPage1[0].imageId == 'imageId1'
+        assertTrue(test_description+ ' - the first image of page 1 is NOT the first image available', imagesPage1[0].imageId == image1Id )
         and: "the second page has the correct number of images"
-        assert (imagesPage2.size() == pageSize)
+        assertTrue (test_description+ ' - page 2 does NOT have the correct number of images',imagesPage2.size() == pageSize)
         and: "the third page has the correct number of images"
-        assert (imagesPage3.size() == privateImagesComplex.size() - offset3)
+        assertTrue (test_description+' - page 3 does NOT have the correct number of images',imagesPage3.size() == totalNumberOfImages - offset3)
         and: "there is no fourth page"
-        assert (imagesPage4.size() == 0)
+        assertTrue (test_description+' - page 4 does NOT have the correct number of images',imagesPage4.size() == 0)
         and: "the first image on the second page is the image immediately after the last image on the first page"
-        assert (Integer.valueOf(imagesPage2[0].imageId[-1]) == Integer.valueOf(imagesPage1[-1].imageId[-1]) + 1)
+        assertTrue (test_description+' - the first image on page 2 is NOT sequential to the last image on page 1',Integer.valueOf(imagesPage2[0].imageId[-1]) == Integer.valueOf(imagesPage1[-1].imageId[-1]) + 1)
         and: "the first image on the third page is the image immediately after the last image on the second page"
-        assert (Integer.valueOf(imagesPage3[0].imageId[-1]) == Integer.valueOf(imagesPage2[-1].imageId[-1]) + 1)
-        and: "the count of total available images is accurate"
-        assert (count == privateImagesComplex.size())
+        assertTrue (test_description+' - the first image on page 3 is not sequential to the last image on page 2',Integer.valueOf(imagesPage3[0].imageId[-1]) == Integer.valueOf(imagesPage2[-1].imageId[-1]) + 1)
+        and: 'the count of total available images is accurate'
+        assertTrue (test_description+' - the number of paged images is different to the total number of images',count == totalNumberOfImages)
         and: "all images are accounted for"
-        assert (count == (imagesPage1.size() + imagesPage2.size() + imagesPage3.size() + imagesPage4.size()))
+        assertTrue (test_description+' - not all images were accounted for',count == (imagesPage1.size() + imagesPage2.size() + imagesPage3.size() + imagesPage4.size()))
+        where: "combinations of image types are"
+        profileResponse ||  image1Id  | biocacheService  | test_description
+                [profile: [scientificName: 'Olympia', uuid: 'profile1', privateImages: privateImagesComplex], opus: [keepImagesPrivate: true, uuid: 'collection1']] ||  'imageId1' | biocacheServiceMockNoImages | "Private images only"
+                [profile: [scientificName: 'Olympia', uuid: 'profile1', privateMode: true, stagedImages: stagedImagesComplex, privateImages: []], opus: [ uuid: 'collection1']] ||  'imageId1' | biocacheServiceMockNoImages | "Staged images only"
     }
 
 
@@ -222,9 +232,8 @@ class ImagePagingSpec extends Specification {
         assert (imagesPage5.size() == 3)
         and: "all images are accounted for"
         assert (count == (imagesPage3.size() + imagesPage4.size()) + imagesPage5.size() + 10)
-
-
     }
+
 
 
 }
