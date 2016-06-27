ce39f4cf5247d2b5190dbd538edda0f2fe39858f
Javier Molina <javier.molina@acsmail.net.au>
2016-06-27 18:24:59 +1000

    #361 - Code cleanup

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -2,8 +2,6 @@ class UrlMappings {
 
     static mappings = {
 
-        "/resource/$action?/$id?(.$format)?" controller: "resource"
-
         "/ping" controller: "ping", action: "ping"
 
         "/tags" controller: "opus", action: "getTags"
@@ -25,12 +23,10 @@ class UrlMappings {
         "/profile/search/taxon/levels" controller: "search", action: "getTaxonLevels"
         "/profile/search/children" controller: "search", action: "getImmediateChildren"
 
-
-
         // This does not follow standard url convention because the document-preview-plugin appends the documentID at the end of it
-        "/opus/$opusId/profile/$profileId/resource/delete/$documentId" controller: "resource", action: [DELETE: "documentDelete"]
-        "/opus/$opusId/profile/$profileId/resource/update/$documentId?(.$format)?" controller: "resource", action: [POST: "documentUpdate"]
-        "/opus/$opusId/profile/$profileId/resource/update" controller: "resource", action: [POST: "documentUpdate"] // This is actually a create
+        "/opus/$opusId/profile/$profileId/resource/delete/$documentId" controller: "profile", action: [DELETE: "documentDelete"]
+        "/opus/$opusId/profile/$profileId/resource/update/$documentId?(.$format)?" controller: "profile", action: [POST: "documentUpdate"]
+        "/opus/$opusId/profile/$profileId/resource/update" controller: "profile", action: [POST: "documentUpdate"] // This is actually a create
         "/opus/$opusId/profile/create" controller: "profile", action: [PUT: "createProfile"]
         "/opus/$opusId/profile/$profileId/duplicate" controller: "profile", action: [PUT: "duplicateProfile"]
         "/opus/$opusId/profile/$profileId/delete" controller: "profile", action: [DELETE: "deleteProfile"]
