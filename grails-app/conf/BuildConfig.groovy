a06cd4c833c0d55ce15febe04c030d838cfe76aa
Javier Molina <Javier.Molina@acsmail.net.au>
2016-11-04 11:02:24 +1100

    - Troubleshooting build.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -101,7 +101,7 @@ grails.project.dependency.resolution = {
 
         //compile ":twitter-bootstrap:3.3.6"
         runtime ":jquery:1.11.1"
-        compile ":asset-pipeline:2.11.0"
+        compile ":asset-pipeline:2.11.2-SNAPSHOT"
         runtime ":angular-annotate-asset-pipeline:2.4.1"
         runtime ":angular-template-asset-pipeline:2.3.0"
 
