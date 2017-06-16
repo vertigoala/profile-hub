3bce17a7a62398825ab0338e8076e55ca0afa052
Simon Bear <simon.bear@csiro.au>
2017-06-17 01:16:57 +1000

    Attempt to make phantomjs travis more reliable take #1

--- karma.conf.js
+++ karma.conf.js
@@ -126,6 +126,10 @@ module.exports = function (config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true
+        singleRun: true,
+
+        // Testing whether phantomjs travis tests are more reliable...
+        browserNoActivityTimeout: 30000,
+        browserDisconnectTolerance: 2
     });
 };
