5823f9fbba1f5a1f986fd7b873631d21677926c1
Simon Bear <simon.bear@csiro.au>
2017-06-16 12:19:43 +1000

    Glossary - can't access glossary #468  - Fix front end unit test to expect call to hub instead of directly to service

--- test/js/specs/services/ProfileServiceSpec.js
+++ test/js/specs/services/ProfileServiceSpec.js
@@ -288,7 +288,7 @@ describe("ProfileService tests", function () {
     it("should invoke the get glossary operation on the PROFILE SERVICE when getGlossary is called", function() {
         service.getGlossary("opus1", "prefix");
 
-        http.expectGET("http://profileService/opus/opus1/glossary/prefix").respond("bla");
+        http.expectGET("/someContext/opus/opus1/glossary/prefix").respond("bla");
     });
 
     it("should invoke the delete glossary item operation on the context root when deleteGlossaryItem is called", function() {
