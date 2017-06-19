23667a56decd1a91498753e13c0f0a5755967524
Ben Richardson <ben.richardson@dpaw.wa.gov.au>
2017-06-19 13:58:43 +0800

    #480 Fixed test.

--- test/integration/au/org/ala/profile/hub/GlossaryControllerSpec.groovy
+++ test/integration/au/org/ala/profile/hub/GlossaryControllerSpec.groovy
@@ -46,7 +46,7 @@ class GlossaryControllerSpec extends IntegrationSpec {
         controller.view()
 
         then:
-        assert controller.modelAndView.model.size() == 16
+        assert controller.modelAndView.model.size() == 17
         assert controller.modelAndView.model.containsKey("logos")
         assert controller.modelAndView.model.containsKey("bannerUrl")
         assert controller.modelAndView.model.containsKey("pageTitle")
