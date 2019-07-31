d1443bb7f484638aa082496642c9bf1acbdee1bc
Rita Chen <Rita.Chen@csiro.au>
2019-07-31 15:06:16 +1000

    Roll back "try to make Travis working again" changes.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -5,28 +5,28 @@ import org.apache.catalina.startup.Tomcat
 
 def ant = new AntBuilder()
 
-//eventCompileEnd = {
-//    println "\n- Starting JasperReports *.jrxml compilation process..."
-//    // define the Jasper Reports Compile Task
-//    ant.taskdef(name:'reportCompile', classname: 'net.sf.jasperreports.ant.JRAntCompileTask', classpath: projectWorkDir)
-//
-//    // remove existing jasper files
-//    ant.delete{
-//        fileset('dir':'grails-app/conf/reports', 'defaultexcludes':'yes'){
-//            include('name':'**/*.jasper')
-//        }
-//    }
-//
-//    // create a temporary directory for use by the jasper compiler
-//    ant.mkdir(dir:'target/jasper')
-//
-//    // compile the reports
-//    ant.reportCompile(srcdir:'grails-app/conf/reports', destdir:'grails-app/conf/reports', tempdir:'target/jasper', keepJava:true, xmlvalidation:true){
-//        include(name:'**/*.jrxml')
-////        include(name:'**/*.jrtx')
-//    }
-//    println "-------\n- JasperReports compilation process finished"
-//}
+eventCompileEnd = {
+    println "\n- Starting JasperReports *.jrxml compilation process..."
+    // define the Jasper Reports Compile Task
+    ant.taskdef(name:'reportCompile', classname: 'net.sf.jasperreports.ant.JRAntCompileTask', classpath: projectWorkDir)
+
+    // remove existing jasper files
+    ant.delete{
+        fileset('dir':'grails-app/conf/reports', 'defaultexcludes':'yes'){
+            include('name':'**/*.jasper')
+        }
+    }
+
+    // create a temporary directory for use by the jasper compiler
+    ant.mkdir(dir:'target/jasper')
+
+    // compile the reports
+    ant.reportCompile(srcdir:'grails-app/conf/reports', destdir:'grails-app/conf/reports', tempdir:'target/jasper', keepJava:true, xmlvalidation:true){
+        include(name:'**/*.jrxml')
+//        include(name:'**/*.jrtx')
+    }
+    println "-------\n- JasperReports compilation process finished"
+}
 
 eventCreateWarStart = { warName, stagingDir ->
     ant.propertyfile(file: "${stagingDir}/WEB-INF/classes/application.properties") {
