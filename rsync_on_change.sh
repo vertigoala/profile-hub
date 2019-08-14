903957b0577f93f495b194f9786f0da8ec145545
Andre Fernandes <andre@vertigo.com.br>
2019-08-14 07:30:19 -0300

    rsync script

---
+++ rsync_on_change.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# sync remote workspace into local path (remote IDE sessions liek VS Code)
+#
+while true
+do 
+  rsync -rlptzv --progress --delete --exclude=.git "ala-helper:/root/projetos/profile-hub" .
+  sleep 2
+done
+
